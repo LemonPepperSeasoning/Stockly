9421489f948f5189065c5983a4d3a983d145faaa
LemonPapperSeasoning <tish386@aucklanduni.ac.nz>
2021-10-04 20:06:49 +1300

    Add multiple card - Needs to implement transition animation - add correct question and answer

--- app/src/main/java/com/larkspur/stockly/Activities/HelpActivity.java
+++ app/src/main/java/com/larkspur/stockly/Activities/HelpActivity.java
@@ -13,45 +13,56 @@ import com.larkspur.stockly.R;
 
 public class HelpActivity extends AppCompatActivity{
 
-    ImageButton expandButton;
-    CardView cardView;
-    ConstraintLayout hiddenView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
 
-        cardView = findViewById(R.id.base_expandableCard);
-        expandButton = findViewById(R.id.expand_button);
-        hiddenView = findViewById(R.id.hidden_laypout);
+        setButtonOnClick(findViewById(R.id.base_expandableCard_1),
+                findViewById(R.id.expand_button_1),
+                findViewById(R.id.hidden_layout_1));
+        setButtonOnClick(findViewById(R.id.base_expandableCard_2),
+                findViewById(R.id.expand_button_2),
+                findViewById(R.id.hidden_layout_2));
+        setButtonOnClick(findViewById(R.id.base_expandableCard_3),
+                findViewById(R.id.expand_button_3),
+                findViewById(R.id.hidden_layout_3));
+        setButtonOnClick(findViewById(R.id.base_expandableCard_4),
+                findViewById(R.id.expand_button_4),
+                findViewById(R.id.hidden_layout_4));
+        setButtonOnClick(findViewById(R.id.base_expandableCard_5),
+                findViewById(R.id.expand_button_5),
+                findViewById(R.id.hidden_layout_5));
+        setButtonOnClick(findViewById(R.id.base_expandableCard_6),
+                findViewById(R.id.expand_button_6),
+                findViewById(R.id.hidden_layout_6));
 
-        expandButton.setOnClickListener(new View.OnClickListener() {
+    }
+
+    private void setButtonOnClick(CardView card,ImageButton button,ConstraintLayout hiddenLayout){
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 // If the CardView is already expanded, set its visibility
                 //  to gone and change the expand less icon to expand more.
-                if (hiddenView.getVisibility() == View.VISIBLE) {
+                if (hiddenLayout.getVisibility() == View.VISIBLE) {
 
-                    // The transition of the hiddenView is carried out
-                    //  by the TransitionManager class.
-                    // Here we use an object of the AutoTransition
-                    // Class to create a default transition.
-                    TransitionManager.beginDelayedTransition(cardView,
-                            new AutoTransition());
-                    hiddenView.setVisibility(View.GONE);
-                    expandButton.setImageResource(R.drawable.unexpand_button);
+
+                    //TODO: Fix this transition. This transition works but its glitchy.
+//                    TransitionManager.beginDelayedTransition(card,
+//                            new AutoTransition());
+                    hiddenLayout.setVisibility(View.GONE);
+                    button.setImageResource(R.drawable.expand_button);
                 }
 
                 // If the CardView is not expanded, set its visibility
                 // to visible and change the expand more icon to expand less.
                 else {
-
-                    TransitionManager.beginDelayedTransition(cardView,
+                    TransitionManager.beginDelayedTransition(card,
                             new AutoTransition());
-                    hiddenView.setVisibility(View.VISIBLE);
-                    expandButton.setImageResource(R.drawable.expand_button);
+                    hiddenLayout.setVisibility(View.VISIBLE);
+                    button.setImageResource(R.drawable.unexpand_button);
                 }
             }
         });
