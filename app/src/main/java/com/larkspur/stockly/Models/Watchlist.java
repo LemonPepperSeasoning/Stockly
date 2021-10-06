c10fef8dc50a8b236590cbb01fa3f0522ca682d1
LemonPapperSeasoning <tish386@aucklanduni.ac.nz>
2021-10-06 14:49:07 +1300

    basic watchlist functionality

---
+++ app/src/main/java/com/larkspur/stockly/Models/Watchlist.java
@@ -0,0 +1,36 @@
+package com.larkspur.stockly.Models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Watchlist implements IWatchlist{
+
+    List<IStock> _watchlist;
+
+    public Watchlist(){
+        _watchlist = new ArrayList<>();
+    }
+
+    @Override
+    public void addStock(IStock stock) {
+        if (_watchlist.contains(stock)){
+            //TODO : Might want to throw exception
+            return;
+        }
+        _watchlist.add(stock);
+    }
+
+    @Override
+    public void removeStock(IStock stock) {
+        if (!_watchlist.contains(stock)){
+            //TODO : Might want to throw exception
+            return;
+        }
+        _watchlist.remove(stock);
+    }
+
+    @Override
+    public List<IStock> getWatchlist() {
+        return _watchlist;
+    }
+}
