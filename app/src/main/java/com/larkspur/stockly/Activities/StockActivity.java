2468588cd65e0de47f96977a1f670b7e5f6538d9
LemonPapperSeasoning <tish386@aucklanduni.ac.nz>
2021-10-07 22:40:02 +1300

    Stock UI without the graph

---
+++ app/src/main/java/com/larkspur/stockly/Activities/StockActivity.java
@@ -0,0 +1,17 @@
+package com.larkspur.stockly.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.larkspur.stockly.R;
+
+public class StockActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stock);
+    }
+
+}
