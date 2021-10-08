4f9bb359aacf56f57cec03464ee86fc147a86e90
jlee842 <jlee842@aucklanduni.ac.nz>
2021-10-08 19:11:49 +1300

    Finished pie chart and portfolio first draft
    Finished pie chart and portfolio first draft. Need to fit into screen. Need add title.

--- app/src/main/java/com/larkspur/stockly/Activities/PortfolioActivity.java
+++ app/src/main/java/com/larkspur/stockly/Activities/PortfolioActivity.java
@@ -12,6 +12,8 @@ import android.text.style.RelativeSizeSpan;
 import android.text.style.StyleSpan;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.PieChart;
@@ -21,6 +23,7 @@ import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
 import com.github.mikephil.charting.formatter.PercentFormatter;
 import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.MPPointF;
 import com.larkspur.stockly.R;
 
 import java.util.ArrayList;
@@ -29,8 +32,12 @@ public class PortfolioActivity extends AppCompatActivity {
 
     DrawerLayout _drawerLayout;
     private PieChart chart;
-    private Typeface tfLight;
-    private Typeface tfRegular;
+    private Typeface tf;
+    private TextView tvX, tvY;
+    private SeekBar seekBarX, seekBarY;
+    protected Typeface tfRegular;
+    protected Typeface tfLight;
+
     protected final String[] parties = new String[] {
             "Party A", "Party B", "Party C", "Party D", "Party E", "Party F", "Party G", "Party H",
             "Party I", "Party J", "Party K", "Party L", "Party M", "Party N", "Party O", "Party P",
@@ -46,15 +53,23 @@ public class PortfolioActivity extends AppCompatActivity {
         setContentView(R.layout.activity_portfolio);
         _drawerLayout = findViewById(R.id.drawer_layout);
 
-        setTitle("HalfPieChartActivity");
+        setTitle("PieChartActivity");
+
+//        tvX = findViewById(R.id.tvXMax);
+//        tvY = findViewById(R.id.tvYMax);
+
+//        seekBarX = findViewById(R.id.seekBar1);
+//        seekBarY = findViewById(R.id.seekBar2);
+//
+//        seekBarX.setOnSeekBarChangeListener(this);
+//        seekBarY.setOnSeekBarChangeListener(this);
 
         chart = findViewById(R.id.doughnut_chart);
-        chart.setBackgroundColor(Color.WHITE);
-
-//        moveOffScreen();
-
         chart.setUsePercentValues(true);
         chart.getDescription().setEnabled(false);
+        chart.setExtraOffsets(5, 10, 5, 5);
+
+        chart.setDragDecelerationFrictionCoef(0.95f);
 
         chart.setCenterTextTypeface(tfLight);
         chart.setCenterText(generateCenterSpannableText());
@@ -70,45 +85,80 @@ public class PortfolioActivity extends AppCompatActivity {
 
         chart.setDrawCenterText(true);
 
-        chart.setRotationEnabled(false);
+        chart.setRotationAngle(0);
+        // enable rotation of the chart by touch
+        chart.setRotationEnabled(true);
         chart.setHighlightPerTapEnabled(true);
 
-        chart.setMaxAngle(180f); // HALF CHART
-        chart.setRotationAngle(180f);
-        chart.setCenterTextOffset(0, -20);
+        // chart.setUnit(" €");
+        // chart.setDrawUnitsInChart(true);
 
-        setData(4, 100);
+//        // add a selection listener
+//        chart.setOnChartValueSelectedListener(this);
+
+//        seekBarX.setProgress(4);
+//        seekBarY.setProgress(10);
 
         chart.animateY(1400, Easing.EaseInOutQuad);
+        // chart.spin(2000, 0, 360);
 
-        Legend l = chart.getLegend();
-        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
-        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
-        l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
-        l.setDrawInside(false);
-        l.setXEntrySpace(7f);
-        l.setYEntrySpace(0f);
-        l.setYOffset(0f);
+//        Legend l = chart.getLegend();
+//        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+//        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.RIGHT);
+//        l.setOrientation(Legend.LegendOrientation.VERTICAL);
+//        l.setDrawInside(false);
+//        l.setXEntrySpace(7f);
+//        l.setYEntrySpace(0f);
+//        l.setYOffset(0f);
 
         // entry label styling
         chart.setEntryLabelColor(Color.WHITE);
         chart.setEntryLabelTypeface(tfRegular);
         chart.setEntryLabelTextSize(12f);
+
+        setData(45, new Float(100));
     }
 
     private void setData(int count, float range) {
+        ArrayList<PieEntry> entries = new ArrayList<>();
 
-        ArrayList<PieEntry> values = new ArrayList<>();
-
-        for (int i = 0; i < count; i++) {
-            values.add(new PieEntry((float) ((Math.random() * range) + range / 5), parties[i % parties.length]));
+        // NOTE: The order of the entries when being added to the entries array determines their position around the center of
+        // the chart.
+        for (int i = 0; i < count ; i++) {
+            entries.add(new PieEntry((float) ((Math.random() * range) + range / 5),
+                    parties[i % parties.length]));
         }
 
-        PieDataSet dataSet = new PieDataSet(values, "Election Results");
+        PieDataSet dataSet = new PieDataSet(entries, "Election Results");
+
+        dataSet.setDrawIcons(false);
+
         dataSet.setSliceSpace(3f);
+        dataSet.setIconsOffset(new MPPointF(0, 40));
         dataSet.setSelectionShift(5f);
 
-        dataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+        // add a lot of colors
+
+        ArrayList<Integer> colors = new ArrayList<>();
+
+        for (int c : ColorTemplate.VORDIPLOM_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.JOYFUL_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.COLORFUL_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.LIBERTY_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.PASTEL_COLORS)
+            colors.add(c);
+
+        colors.add(ColorTemplate.getHoloBlue());
+
+        dataSet.setColors(colors);
         //dataSet.setSelectionShift(0f);
 
         PieData data = new PieData(dataSet);
@@ -118,6 +168,8 @@ public class PortfolioActivity extends AppCompatActivity {
         data.setValueTypeface(tfLight);
         chart.setData(data);
 
+        // undo all highlights
+        chart.highlightValues(null);
 
         chart.invalidate();
     }
@@ -133,7 +185,6 @@ public class PortfolioActivity extends AppCompatActivity {
         s.setSpan(new ForegroundColorSpan(ColorTemplate.getHoloBlue()), s.length() - 14, s.length(), 0);
         return s;
     }
-
 
     public void clickMenu(View view){
         MainActivity.openDrawer(_drawerLayout);
