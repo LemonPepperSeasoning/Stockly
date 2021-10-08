0581e8fd5ecbe7ae2fa83b3541f0173a657ac9af
jlee842 <jlee842@aucklanduni.ac.nz>
2021-10-08 14:18:46 +1300

    Added list item and modified listactivity
    I added a extra for the list view in listActivity and fixed the widget.

--- app/src/main/java/com/larkspur/stockly/Activities/ListActivity.java
+++ app/src/main/java/com/larkspur/stockly/Activities/ListActivity.java
@@ -1,17 +1,56 @@
 package com.larkspur.stockly.Activities;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.larkspur.stockly.R;
 
 public class ListActivity extends AppCompatActivity {
 
+    DrawerLayout _drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        _drawerLayout = findViewById(R.id.drawer_layout);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
     }
 
+    public void clickMenu(View view){
+        MainActivity.openDrawer(_drawerLayout);
+    }
+
+    public void clickCloseSideMenu(View view){
+        MainActivity.closeDrawer(_drawerLayout);
+    }
+
+    public void clickHome(View view){
+        MainActivity.redirectActivity(this,MainActivity.class);
+    }
+
+    public void clickPortfolio(View view){
+        recreate();
+    }
+
+    public void clickWatchlist(View view){
+        MainActivity.redirectActivity(this,WatchlistActivity.class);
+    }
+
+    public void clickSettings(View view){
+        MainActivity.redirectActivity(this,SettingsActivity.class);
+    }
+
+    public void clickHelp(View view) {
+        MainActivity.redirectActivity(this,HelpActivity.class);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        MainActivity.closeDrawer(_drawerLayout);
+    }
 }
