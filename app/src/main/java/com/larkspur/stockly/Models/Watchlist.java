826ab13e1403bcae908d6306fe006caddcf6b240
Alan <69041772+forestsky1@users.noreply.github.com>
2021-10-11 22:10:49 +1300

    added a map into watchlist to track stocks by their symbols
    problem is that everytime we load in stocks through firebase - they are new stock objects so there is no way to refer back to the old ones other than storing them - which is expensive

--- app/src/main/java/com/larkspur/stockly/Models/Watchlist.java
+++ app/src/main/java/com/larkspur/stockly/Models/Watchlist.java
@@ -1,16 +1,19 @@
 package com.larkspur.stockly.Models;
 import android.widget.ListView;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Watchlist implements IWatchlist {
 
-    private List<IStock> _watchlist;
+    private Map<String,IStock> _watchlist;
     private static Watchlist single_instance = null;
 
 
     private Watchlist() {
-        _watchlist = new ArrayList<>();
+        _watchlist = new HashMap<>();
     }
 
     public static Watchlist getInstance() {
@@ -23,30 +26,31 @@ public class Watchlist implements IWatchlist {
 
     @Override
     public void addStock(IStock stock) {
-        if (_watchlist.contains(stock)) {
+        if (_watchlist.containsKey(stock.getSymbol())) {
             //TODO : Might want to throw exception
             return;
         }
-        _watchlist.add(stock);
+        _watchlist.put(stock.getSymbol(),stock);
     }
 
     @Override
     public void removeStock(IStock stock) {
-        if (!_watchlist.contains(stock)){
+        if (!_watchlist.containsKey(stock.getSymbol())){
             //TODO : Might want to throw exception
             return;
         }
-        _watchlist.remove(stock);
+        _watchlist.remove(stock.getSymbol());
     }
 
     @Override
     public List<IStock> getWatchlist() {
-        return _watchlist;
+        List<IStock> stockList = new ArrayList<>(_watchlist.values());
+        return stockList;
     }
 
     @Override
     public boolean hasStock(IStock stock) {
-        return _watchlist.contains(stock);
+        return _watchlist.containsKey(stock.getSymbol());
     }
 
 }
